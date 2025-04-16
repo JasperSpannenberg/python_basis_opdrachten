8d5b84c04554a6d1faa01f39450be1455647415b
Jasper Spannenberg <97104445@st.deltion.nl>
2025-04-16 15:35:38 +0200

    Update opdr_1 (9).py

--- opdrachten/095_modules/opdr_1/opdr_1 (9).py
+++ opdrachten/095_modules/opdr_1/opdr_1 (9).py
@@ -14,3 +14,30 @@ def schrijf_naar_csv(bestandsnaam, data):
     with open(bestandsnaam, "w", encoding="utf-8") as file:
         for regel in data:
             file.write(",".join(regel) + "\n")
+
+from my_modules import personen
+
+personenlijst = [
+    {"voornaam": "Jan", "tussenvoegsel": "van der", "achternaam": "Vliet", "plaats": "Amsterdam"},
+    {"voornaam": "Jan Jaap", "tussenvoegsel": "", "achternaam": "Siewers", "plaats": "Delft"},
+    {"voornaam": "Piet", "tussenvoegsel": "de", "achternaam": "Vries", "plaats": "Leiden"},
+    {"voornaam": "Griet", "tussenvoegsel": "van der", "achternaam": "Pol", "plaats": "Drachten"}
+]
+
+# Test: filter op voornaam begint met 'ja'
+print("Filter op voornaam 'ja':")
+resultaten = personen.filter(personenlijst, "voornaam", "ja")
+for naam in resultaten:
+    print(naam)
+
+# Test: filter op voornaam begint met 'pie'
+print("\nFilter op voornaam 'pie':")
+resultaten = personen.filter(personenlijst, "voornaam", "pie")
+for naam in resultaten:
+    print(naam)
+
+# Test: filter op plaats begint met 'd'
+print("\nFilter op plaats 'd':")
+resultaten = personen.filter(personenlijst, "plaats", "d")
+for naam in resultaten:
+    print(naam)
