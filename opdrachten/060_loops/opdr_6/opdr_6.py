9f813eb88c1e09055a5d7fe95cbd3ccd1b0eee37
Jasper Spannenberg <97104445@st.deltion.nl>
2025-04-16 14:42:49 +0200

    Update opdr_6.py

--- opdrachten/060_loops/opdr_6/opdr_6.py
+++ opdrachten/060_loops/opdr_6/opdr_6.py
@@ -1,9 +1,27 @@
 # Opdracht 3 input functie
-# Naam student:
-# Groep:
+# Naam student: Jasper Spannenberg
+# Groep: IT2B
 
-# Hier komt je code...
+# Originele lijst
+pizzas = ['margharita', 'calzone', 'verdi', 'olivio', 'quattro stagioni']
 
+# Sorteer de lijst op alfabet
+pizzas.sort()
+
+# Voeg een pizza toe (bijvoorbeeld 'yo-favorito')
+pizzas.append('yo-favorito')
+
+# Verwijder de minst lekkere pizza (bijvoorbeeld 'olivio')
+pizzas.remove('olivio')
+
+# Print de eerste 3 pizza's
+print(pizzas[:3])
+
+# Print alleen de middelste pizza (lijst heeft nu 5 items, dus index 2 is midden)
+print([pizzas[len(pizzas)//2]])
+
+# Print de laatste 3 pizza's
+print(pizzas[-3:])
 # Hier start de for-loop
 
 my_list = []
