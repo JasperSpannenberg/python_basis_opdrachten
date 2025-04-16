eed65da29b6fa4b793fceed5728ab043246cc1fe
Jasper Spannenberg <97104445@st.deltion.nl>
2025-04-16 13:45:11 +0200

    Create opdr_1 (2).py

---
+++ opdrachten/030_berekeningen/opdr_1/opdr_1 (2).py
@@ -0,0 +1,16 @@
+# Opdracht 1 berekeningen
+# Naam student: Jasper Spannenberg
+# Groep: IT2B
+
+joe = 23
+bob = 15
+fred = 22
+
+# Berekeningen
+totaal = joe + bob + fred
+gemiddelde = totaal / 3
+
+# Gegevens afdrukken
+print(f"Dit weten we over joe ({joe} jaar), bob ({bob} jaar) en fred ({fred} jaar):")
+print(f"De totale waarde van alle leeftijden bij elkaar opgeteld = {totaal}")
+print(f"De gemiddelde leeftijd van deze boys is: {gemiddelde}")
