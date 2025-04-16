fb1380b242bc91031e46f54119ddca16254bb914
Jasper Spannenberg <97104445@st.deltion.nl>
2025-04-16 13:37:44 +0200

    Create opdr_2 (1).py

---
+++ opdrachten/020_tekstfuncties/opdr_2/opdr_2 (1).py
@@ -0,0 +1,14 @@
+# Opdracht 1
+# Naam student: Jasper Spannenberg
+# Groep: IT2B
+
+my_string = "Tinus gaat op zijn tandem naar de hottentottententoonstelling"
+
+# Zet de string om naar kleine letters zodat we geen onderscheid maken tussen 'T' en 't'
+t_count = my_string.lower().count('t')
+
+print(f'De letter "t" komt {t_count}x voor in my_string')
+
+my_list = ...
+
+print()
