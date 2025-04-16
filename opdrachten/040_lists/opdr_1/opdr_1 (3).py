d857d0802e3406891406f53a5d1225c7a93a9de2
Jasper Spannenberg <97104445@st.deltion.nl>
2025-04-16 13:56:08 +0200

    Create opdr_1 (3).py

---
+++ opdrachten/040_lists/opdr_1/opdr_1 (3).py
@@ -0,0 +1,26 @@
+# Opdracht 1 lists
+# Naam student: Jasper Spannenberg
+# Groep: IT2B
+
+# Maak een lijst om de dictionaries op te slaan
+lijst = []
+
+# Maak 4 dictionaries met gegevens van personen
+persoon1 = {"id": 0, "voornaam": "Jan", "achternaam": "Jansen"}
+persoon2 = {"id": 1, "voornaam": "Emma", "achternaam": "Peters"}
+persoon3 = {"id": 2, "voornaam": "Lucas", "achternaam": "Verbeek"}
+persoon4 = {"id": 3, "voornaam": "Sara", "achternaam": "de Vries"}
+
+# Voeg de dictionaries toe aan de lijst
+lijst.append(persoon1)
+lijst.append(persoon2)
+lijst.append(persoon3)
+lijst.append(persoon4)
+
+# Print de volledige naam van de 2e persoon (index 1)
+print(lijst[1]["voornaam"], lijst[1]["achternaam"])
+mylist = ...
+dict_1 = ...
+dict_2 = ...
+dict_3 = ...
+dict_4 = ...
