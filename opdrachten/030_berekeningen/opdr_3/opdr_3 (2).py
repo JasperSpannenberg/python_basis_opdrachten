aec7bf078436f73605eb4667478eb962836e55b8
Jasper Spannenberg <97104445@st.deltion.nl>
2025-04-16 13:49:44 +0200

    Create opdr_3 (2).py

---
+++ opdrachten/030_berekeningen/opdr_3/opdr_3 (2).py
@@ -0,0 +1,11 @@
+# Opdracht 3 tekstfuncties
+# Naam student: Jasper Spannenberg
+# Groep: IT2B
+
+# Definieer de waarden voor x
+x_values = [1, 2, 0]
+
+# Test voor elke waarde van x
+for x in x_values:
+    y = 4 * (x ** 3) - 2 * (x ** 2) - 1  # De formule voor y
+    print(f"Voor x = {x}, de uitkomst is: {y}")
