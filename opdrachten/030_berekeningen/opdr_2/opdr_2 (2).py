aa38eede1f06370415d019f20e47ca5890a4f627
Jasper Spannenberg <97104445@st.deltion.nl>
2025-04-16 13:47:02 +0200

    Create opdr_2 (2).py

---
+++ opdrachten/030_berekeningen/opdr_2/opdr_2 (2).py
@@ -0,0 +1,36 @@
+# Opdracht 2 berekeningen
+# Naam student: Jasper Spannenberg
+# Groep: IT2B
+
+# Omrekeningen
+c = -12
+f = 102
+
+# Celsius naar Fahrenheit
+f_from_c = (c * 1.8) + 32
+
+# Fahrenheit naar Celsius
+c_from_f = (f - 32) / 1.8
+
+# Resultaten afdrukken
+print(f"{c} graden Celsius is gelijk aan {f_from_c:.1f} graden Fahrenheit")
+print(f"{f} graden Fahrenheit is gelijk aan {c_from_f:.1f} graden Celsius")
+
+# Nieuwe waarden
+c = 62.2
+f = 96
+
+# Celsius naar Fahrenheit
+f_from_c = (c * 1.8) + 32
+
+# Fahrenheit naar Celsius
+c_from_f = (f - 32) / 1.8
+
+# Resultaten afdrukken
+print(f"{c} graden Celsius is gelijk aan {f_from_c:.1f} graden Fahrenheit")
+print(f"{f} graden Fahrenheit is gelijk aan {c_from_f:.1f} graden Celsius")
+
+c = ...
+f = ...
+
+print()
