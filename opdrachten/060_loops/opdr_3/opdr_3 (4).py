7b8fe0a81349b31845347f0f72974d5612000be3
Jasper Spannenberg <97104445@st.deltion.nl>
2025-04-16 14:35:48 +0200

    Create opdr_3 (4).py

---
+++ opdrachten/060_loops/opdr_3/opdr_3 (4).py
@@ -0,0 +1,18 @@
+# Opdracht 3 input functie
+# Naam student:Jasper Spannenberg
+# Groep:IT2B
+
+# Maak een lege lijst
+resultaat = []
+
+# For-loop van 3 t/m 78, met stappen van 3
+for i in range(3, 81, 3):
+    berekening = (i ** 2) / 3
+    resultaat.append(berekening)
+
+# Print de lijst
+print(resultaat)
+
+# Hier start de for-loop
+
+my_list = []
