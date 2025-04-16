12f90d663043c0641438964dc8f13dcb9139ea01
Jasper Spannenberg <97104445@st.deltion.nl>
2025-04-16 13:41:13 +0200

    Create opdr_3 (1).py

---
+++ opdrachten/020_tekstfuncties/opdr_3/opdr_3 (1).py
@@ -0,0 +1,12 @@
+# Opdracht 3 tekstfuncties
+# Naam student:Jasper Spannenberg
+# Groep:IT2B
+
+print("    *   " * 5)
+print("   ***  " * 5)
+print("  ******" * 5)
+print(" ********" * 5)
+print("***********" * 5)
+print("    ***  " * 5)
+print("    ***  " * 5)
+print("    ***  " * 5)
