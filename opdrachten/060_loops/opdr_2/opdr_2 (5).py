cfa9d6d43703e2019b0df8757f8bc77b56581f8a
Jasper Spannenberg <97104445@st.deltion.nl>
2025-04-16 14:35:46 +0200

    Create opdr_2 (5).py

---
+++ opdrachten/060_loops/opdr_2/opdr_2 (5).py
@@ -0,0 +1,18 @@
+# Opdracht 2 loops
+# Naam student:Jasper Spannenberg
+# Groep: IT2B
+
+# Maak een lege lijst
+getallen = []
+
+# Gebruik een for-loop met range om de getallen toe te voegen
+for i in range(10, 51, 10):  # start bij 10, eindig vóór 51, stap van 10
+    getallen.append(i)
+
+# Print de lijst
+print(getallen)
+
+# Hier start de for-loop
+
+my_list = []
+
