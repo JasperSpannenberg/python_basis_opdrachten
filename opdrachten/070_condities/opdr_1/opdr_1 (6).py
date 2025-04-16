efeeaa7210b135d92d385ec671bd4620783c8175
Jasper Spannenberg <97104445@st.deltion.nl>
2025-04-16 14:45:24 +0200

    Create opdr_1 (6).py

---
+++ opdrachten/070_condities/opdr_1/opdr_1 (6).py
@@ -0,0 +1,19 @@
+# Opdracht 1 condities
+# Naam student: Jasper Spannenberg
+# Groep:IT2B
+
+# Maak een lege lijst
+groter_dan_vier = []
+
+# Loop van 1 t/m 10
+for i in range(1, 11):
+    if i > 4:
+        groter_dan_vier.append(i)
+
+# Print de lijst
+print(groter_dan_vier)
+
+my_list = []
+
+#for loop
+#if statement
